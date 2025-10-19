342dff6c2aee357c4a0e13a5c5de7f0345e3cd46
nikhil <paterinikhil0888@gmail.com>
2025-10-20 00:51:26 +0530

    login commit

--- taaja/frontend/lib/main.dart
+++ taaja/frontend/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login & Register Demo',
+      title: 'Login & Registers Demo',
       home: const LoginPage(),
     );
   }
